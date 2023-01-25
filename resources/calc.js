c179f2907b15dd99d697e068177c992518bbf640
Herasymenko <116513411+GeraNata@users.noreply.github.com>
2023-01-25 21:40:37 +0200

    Add files via upload

---
+++ resources/calc.js
@@ -0,0 +1,26 @@
+document.querySelector('.calc-calculate').addEventListener('click', function(){
+    const num1=+document.querySelector('[name="num1"]').value;
+    const num2=+document.querySelector('[name="num2"]').value;
+    const op=document.querySelector('[name="op"]').value;
+
+    let result;
+if (op === 'plus') {
+    result = num1 + num2;
+} else if (op === 'minus') {
+    result = num1 - num2;
+}
+else if (op === 'divide') {
+    result = num1 / num2;
+}
+else if (op === 'multiple') {
+    result = num1 * num2;
+}
+else{
+console.error('wrong op');
+alert('wrong');
+}
+
+    document.querySelector('.calc-result').value = result;
+ 
+});
+
